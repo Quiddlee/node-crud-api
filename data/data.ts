e536ebbc728b1bd17ebe36e28d9aadca88cfacd1
Bohdan <bogdanscherbinadev@gmail.com>
2024-01-29 14:56:27 +0200

    feat: add test endpoint

---
+++ data/data.ts
@@ -0,0 +1,10 @@
+import { UserList } from '../types/types';
+
+export const users: UserList = [
+  {
+    id: '1',
+    age: 19,
+    username: 'Quiddle',
+    hobbies: ['programming'],
+  },
+];
--- server.ts
+++ server.ts
@@ -1,1 +1,22 @@
 import 'dotenv/config';
+
+import http from 'http';
+import { URL } from 'url';
+
+import { users } from './data/data';
+
+const server = http.createServer((req, res) => {
+  const baseURL = `http://${req.headers.host}/`;
+  const endpoint = req?.url ?? '/api';
+  const { pathname } = new URL(endpoint, baseURL);
+
+  if (pathname === '/api/users')
+    res
+      .writeHead(200, { 'Content-type': 'application/json' })
+      .end(JSON.stringify(users));
+});
+
+const port = Number(process.env.PORT);
+server.listen(port, '127.0.0.1', () => {
+  console.log(`App running on port ${port}...`);
+});
