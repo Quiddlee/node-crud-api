0eaab59a4d8be04448c9c74c77fd8080a4038fac
Bohdan <bogdanscherbinadev@gmail.com>
2024-02-03 16:11:13 +0200

    refactor: re-arrange server.ts code

--- server.ts
+++ server.ts
@@ -11,15 +11,14 @@ import {
 import Api from './utils/Api';
 
 const api = new Api();
-
 const port = Number(process.env.PORT);
 const host = process.env.HOST;
-api.listen(port, host, () => {
-  // eslint-disable-next-line no-console
-  console.log(`App running on port ${port}...`);
-});
 
 api.use(validateId);
 api.route('/api/users').get(getUserList).post(createUser);
 api.route('/api/users/:id').get(getUser).put(updateUser).delete(deleteUser);
 api.use(notFound);
+
+api.listen(port, host, () => {
+  process.stdout.write(`App running on port ${port}...`);
+});
