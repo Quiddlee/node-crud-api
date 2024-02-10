242c42b0b7f6c08f0febed3d00067957601a8002
Bohdan <bogdanscherbinadev@gmail.com>
2024-02-10 19:45:19 +0200

    feat: implement multi mode to run multiple instances of the app

--- server.ts
+++ server.ts
@@ -1,4 +1,8 @@
 import 'dotenv/config';
+import cluster from 'cluster';
+import http from 'http';
+import { availableParallelism } from 'node:os';
+
 import {
   createUser,
   deleteUser,
@@ -7,19 +11,83 @@ import {
   notFound,
   updateUser,
 } from './controllers/usersController';
+import db from './db/db';
 import { Routes } from './types/enums';
+import { WorkerMessage } from './types/types';
 import App from './utils/app';
+import forwardRequest from './utils/forwardRequest';
+import isMulti from './utils/isMulti';
 import { validateId } from './utils/validateId';
 
-const app = new App();
+const { isPrimary } = cluster;
+
+const numCPUs = availableParallelism();
 const port = Number(process.env.PORT);
 const host = process.env.HOST;
+const workerPorts: number[] = [];
+const isMultiMode = isMulti();
+const isMasterProcess = isPrimary && isMultiMode;
+let currentWorkerNum = 0;
 
-app.use(validateId);
-app.route(Routes.USERS).get(getUserList).post(createUser);
-app.route(Routes.USERS_ID).get(getUser).put(updateUser).delete(deleteUser);
-app.use(notFound);
+if (isMasterProcess) {
+  // Master process // load balancer code
+  http
+    .createServer((req, res) => {
+      const workerCreds = {
+        hostname: host,
+        port: workerPorts[currentWorkerNum],
+      };
 
-app.listen(port, host, () => {
-  process.stdout.write(`App running on port ${port}...`);
-});
+      forwardRequest(req, res, workerCreds);
+
+      if (currentWorkerNum === workerPorts.length - 1) {
+        currentWorkerNum = 0;
+        return;
+      }
+      currentWorkerNum += 1;
+    })
+    .listen(port, host, () => {
+      process.stdout.write(`The load balancer is running on port ${port}...\n`);
+    });
+
+  for (let i = 1; i < numCPUs; i += 1) {
+    const workersPort = port + i;
+    const worker = cluster.fork({ PORT: workersPort });
+    workerPorts.push(workersPort);
+
+    worker.on('message', (msg: WorkerMessage) => {
+      if (!('command' in msg)) return;
+
+      const workerArgs = msg.args;
+      const dbAction = db[msg.command];
+      dbAction(...workerArgs).then((data) => {
+        worker.send({ res: data });
+      });
+    });
+  }
+} else if (isMultiMode) {
+  // Worker's code
+  const app = new App();
+  const workerPort = Number(process.env.PORT);
+
+  app.use(validateId);
+  app.route(Routes.USERS).get(getUserList).post(createUser);
+  app.route(Routes.USERS_ID).get(getUser).put(updateUser).delete(deleteUser);
+  app.use(notFound);
+
+  app.listen(workerPort, host, () => {
+    process.stdout.write(`App running on port ${workerPort}...\n`);
+  });
+} else {
+  // Base app
+  const app = new App();
+
+  app.use(validateId);
+  app.route(Routes.USERS).get(getUserList).post(createUser);
+  app.route(Routes.USERS_ID).get(getUser).put(updateUser).delete(deleteUser);
+  app.use(notFound);
+
+  app.listen(port, host, () => {
+    process.stdout.write(`App running on port ${port}...`);
+  });
+}
---
+++ utils/forwardRequest.ts
@@ -0,0 +1,29 @@
+import http from 'http';
+
+import { Res } from './response';
+import { Req, WorkerRequestData } from '../types/types';
+
+/**
+ * Forwards the request to the worker with the given data.
+ * @param {Req} req - The incoming request object.
+ * @param {Res} res - The outgoing response object.
+ * @param {WorkerRequestData} workerData - The data of the worker to forward the request to.
+ */
+const forwardRequest = (req: Req, res: Res, workerData: WorkerRequestData) => {
+  const options = {
+    hostname: workerData.hostname,
+    port: workerData.port,
+    path: req.url,
+    method: req.method,
+    headers: req.headers,
+  };
+
+  const proxy = http.request(options, (workerRes) => {
+    res.writeHead(workerRes.statusCode as number, workerRes.headers);
+    workerRes.pipe(res);
+  });
+
+  req.pipe(proxy);
+};
+
+export default forwardRequest;
