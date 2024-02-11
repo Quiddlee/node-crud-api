555946ab9cb852b55d9faa9567fc347d4fe0544d
Bohdan <bogdanscherbinadev@gmail.com>
2024-02-11 16:23:31 +0200

    refactor: add JSDoc comment to validateId fn

--- lib/utils/validateId.ts
+++ lib/utils/validateId.ts
@@ -3,6 +3,11 @@ import * as uuid from 'uuid';
 import { StatusCode } from '../../types/enums';
 import { ExtendedReq, ExtendedRes } from '../../types/types';
 
+/**
+ * Validates the id in the request route and sends a JSON response with a 400 status code if it is not a valid uuid.
+ * @param {ExtendedReq} req - The incoming request object, which contains the id in the route property.
+ * @param {ExtendedRes} res - The outgoing response object, which is used to send the JSON response.
+ */
 export const validateId = (req: ExtendedReq, res: ExtendedRes) => {
   const id = req.route?.id;
 
